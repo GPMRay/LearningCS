c7faa5196fe06145803799122eb15af40a9b9d69
GPMRay <71682126+GPMRay@users.noreply.github.com>
2021-04-03 14:15:24 -0500

    Create sumCalc2.cs

---
+++ sumCalc2.cs
@@ -0,0 +1,34 @@
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            // This is calling Calculate which will return 'result' in the Console.WriteLine. If we only wrote 'Calculate();' it would still go to the Calculate method, do the math, but not show anything on the scren.
+            Console.WriteLine(Calculate());
+            Console.Read();
+        }
+
+        public static int Calculate()
+        {
+
+            Console.WriteLine("Enter the first number");
+            string sInput1 = Console.ReadLine();
+            Console.WriteLine("Enter the second number");
+            string sInput2 = Console.ReadLine();
+
+            // This converts the string 'sInput1' and 'sInput2' to an int by using Parse.
+            int num1 = int.Parse(sInput1); 
+            int num2 = int.Parse(sInput2);
+
+            int result = num1 + num2;
+
+
+            return result;
+        }
+
+    }
+}
