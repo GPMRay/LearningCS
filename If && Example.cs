8816f7617444f8d412982de8fcbafc78364510c9
GPMRay <71682126+GPMRay@users.noreply.github.com>
2021-04-03 19:41:06 -0500

    Create If && Example.cs

---
+++ If && Example.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isAdmin = false;
+            bool isRegistered = true; // In this example this is always true
+            string userName = ""; // A place holder. Line 17-21 will not run if the user does not enter a value
+            Console.WriteLine("Please enter your username"); // Inputs for a username
+            userName = Console.ReadLine(); // Saves the input to userName
+
+            if (isRegistered && userName != "" && userName.Equals("admin")) // Checks to ensure all the variable specifed are true (except for userName). Code will not run below if no username is specified.
+            {
+                Console.WriteLine("Hi there, registered user");
+
+                Console.WriteLine("Hi there, " + userName); // Returns the user input for variable userName
+
+                Console.WriteLine("Hi there, Admin");
+            }
+            else
+            {
+                Console.WriteLine("You failed to specify a username or an error occured!"); // Any of the variables specified on line 15 do not match, this message will display
+            }
+            Console.Read(); // Pauses the program
+        }
+    }
+}
